5c355ffb614b2b8c2afd6709fa93bc36ee95b510
rvasquez <rvasquez1@gmail.com>
2020-12-23 20:24:55 -0500

    fixed the time showing in the blocks and got the colors right

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ $(document).ready(function () {
   // add css clases acording to the time
 
   $("#currentDay").text(moment().format("MMMM Do YYYY"));
-var varTime = 9;
+var varTime = " AM";
+var textTime = 8;
+var realTime = 9;
   //get current hour
  
- var currentHour = moment().format('LT');
+ var currentHour = parseInt(moment().format('HH'))
  console.log(currentHour);
 
 
@@ -21,35 +23,65 @@ function init(){
 createPage();
 
 
-
 }
 
+$("textarea").each(function () {
+    var name = parseInt($(this).attr("value"));
+    console.log(name + "name");
+    console.log(currentHour + "currenthour");
+    
 
+    if (name < currentHour) {
+        $(this).addClass("past");
+        console.log($(this));
+    }
+
+
+    if (name > currentHour) {
+        $(this).addClass("future")
+        console.log("2");
+    }
+
+    if (name === currentHour) {
+        $(this).addClass("present")
+        console.log("3");
+    }
+
+
+})
 
 function createPage(){
 
-  for (i = 9; i <= 17; i++) {
-    console.log(varTime);   
-    varTime = i;
+
+  for (i = 0; i < 9; i++) {
+
+    textTime++;
+    realTime++;
+
+    if (i >= 3) varTime = " PM";
+    if (i === 4) textTime = 1;
+    
     var divRow = $("<div>");
     divRow.addClass("row time-block");
-    divRow.attr("id", "hour-" + varTime);
+    divRow.attr("id", "hour-" + realTime);
     var divHour = $("<div>");
-    divHour.addClass("col-md-1 hour p-3");
-    divHour.text(varTime);
+    divHour.addClass("col-md-1 hour p-1");
+    divHour.text(textTime + varTime);
     var textHour = $("<textarea>");
+    textHour.attr("value", realTime);
     textHour.addClass("col-md-10 description");
+    setColor(textHour, currentHour, realTime);
+
     var btnHour = $("<button>");
     btnHour.addClass("btn saveBtn col-md-1");
-    btnHour.attr("value", varTime);
+    btnHour.attr("value", realTime);
     var btnIcon =$("<i>");
     btnIcon.addClass("fas fa-save");
 
-
     $(divRow).appendTo(".container");
-    $(divHour).appendTo("#hour-" + varTime);
-    $(textHour).appendTo("#hour-" + varTime);
-    $(btnHour).appendTo("#hour-" + varTime);
+    $(divHour).appendTo("#hour-" + realTime);
+    $(textHour).appendTo("#hour-" + realTime);
+    $(btnHour).appendTo("#hour-" + realTime);
     $(btnIcon).appendTo(btnHour);
 
   }
@@ -67,6 +99,12 @@ function createPage(){
           
 });
 
+function setColor(){
+
+
+
+
+}
 
 
 });
